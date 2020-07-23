fb63f1428f6c71dee85e2866b526eed8f9ecb541
shalongteng <1131790681@qq.com>
2020-07-23 22:04:56 +0800

    spring

---
+++ src/main/java/com/slt/java_basics/spring/v1/TimeProxy.java
@@ -0,0 +1,13 @@
+package com.slt.java_basics.spring.v1;
+
+public class TimeProxy {
+
+    public void before() {
+        System.out.println("method start.." + System.currentTimeMillis());
+    }
+
+    public void after() {
+        System.out.println("method stop.." + System.currentTimeMillis());
+    }
+
+}
