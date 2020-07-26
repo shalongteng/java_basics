a365a82df783d509516ef5db4ece2eecae5f4880
shalongteng <1131790681@qq.com>
2020-07-26 21:33:58 +0800

    map

---
+++ src/main/java/com/slt/java_basics/Collections/map/Demo01.java
@@ -0,0 +1,13 @@
+package com.slt.java_basics.Collections.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+    }
+}
