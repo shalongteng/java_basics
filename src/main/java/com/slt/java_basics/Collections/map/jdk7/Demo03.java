22fedf83985df527b9c56b61b7ebaa06db8e0fcb
yuyanan <yuyanan@handongkeji.com>
2020-07-27 18:19:47 +0800

    asm

---
+++ src/main/java/com/slt/java_basics/Collections/map/jdk7/Demo03.java
@@ -0,0 +1,25 @@
+package com.slt.java_basics.Collections.map.jdk7;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 多线程下[HashMap]的问题
+ *  1、多线程put操作后，get操作导致死循环。
+ *  2、多线程put非NULL元素后，get操作得到NULL值。
+ *  3、多线程put操作，导致元素丢失。
+ */
+public class Demo03 {
+
+    /**
+     *
+     */
+    @Test
+    public void testMap2(){
+        /**
+         */
+        System.out.println(00);
+    }
+}
